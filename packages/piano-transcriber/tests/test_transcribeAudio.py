7d9ef934bc06c8b64ad8352d6fbe737f17f056b0
MengLinMaker <menglinmaker@gmail.com>
2024-12-03 21:58:18 +1100

    test: script to run all test in package

--- packages/piano-transcriber/tests/test_transcribeAudio.py
+++ packages/piano-transcriber/tests/test_transcribeAudio.py
@@ -12,17 +12,22 @@ def transcribeFileSnapshot(audioFileName):
 def test_transcribe_mp3(snapshot):
     assert transcribeFileSnapshot("example.mp3") == snapshot
 
+
 def test_transcribe_webm(snapshot):
     assert transcribeFileSnapshot("example.webm") == snapshot
+
 
 def test_transcribe_ogg(snapshot):
     assert transcribeFileSnapshot("example.ogg") == snapshot
 
+
 def test_transcribe_flac(snapshot):
-    assert transcribeFileSnapshot( "example.flac") == snapshot
+    assert transcribeFileSnapshot("example.flac") == snapshot
+
 
 def test_transcribe_m4a(snapshot):
     assert transcribeFileSnapshot("example.m4a") == snapshot
+
 
 def test_transcribe_opus(snapshot):
     assert transcribeFileSnapshot("example.opus") == snapshot
